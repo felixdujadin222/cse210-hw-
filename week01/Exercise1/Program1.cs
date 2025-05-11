e2f8af3036791a1ea2ea995cafb83510e54afb37
felixdujadin222 <felixdujadin22@gmail.com>
2025-05-11 20:13:23 +0100

    Finished prep 1

---
+++ week01/Exercise1/Program1.cs
@@ -0,0 +1,44 @@
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      //int x = 5; 
+     // Console.WriteLine(x);
+      //Console.WriteLine("Hello World!");
+     // Console.Write("What is your favorite color? ");
+     // string color = Console.ReadLine();
+     // Console.WriteLine($"Your favorite color is {color}");
+
+      //Declear Y
+      //int y  = 3;
+      //int b  = 1;
+     // if (b < y)
+     // {
+        // Console.WriteLine("y is greater than b");
+
+         //STRING DECLARATION
+        // string school = "IDAHO";
+         //{
+           // Console.WriteLine($" My school is {school}");
+         //}
+
+        {
+            Console.Write("What is you first name? ");
+            string first = Console.ReadLine();
+            Console.Write("What is your Last name? ");
+            string last = Console.ReadLine();
+            Console.WriteLine($"Your first name is {last}, {first} {last}");
+        }
+      
+    } 
+}
+
+
+
+
+
+
+
